59e399217a312645f558d2281e0d8fa3597db42f
Alex Mohylivskyi <alex.mohylivskyi.ua@gmail.com>
2026-03-13 01:53:01 +0100

    Chest improvements

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -2,22 +2,26 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour, IDamagable
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Vector2 knockBack = new Vector2(0, 5);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private Animator anim => GetComponentInChildren<Animator>();
+    private Rigidbody2D rb => GetComponent<Rigidbody2D>();
+    private Entity_VFX vfx => GetComponent<Entity_VFX>();
 
     public void TakeDamage(float damage, Transform damageDealer)
     {
         // throw new System.NotImplementedException();
         Debug.Log("chest attacked");
-        GetComponentInChildren<Animator>().SetBool("chestOpen", true);
+        vfx.PlayOnDamageVFX();
+        anim.SetBool("chestOpen", true);
+        
+        
+        if (rb != null)
+        {
+            rb.linearVelocity = knockBack;
+            rb.angularVelocity = Random.Range(-200f, 200f);
+            
+            // Drop items
+        }
     }
 }
